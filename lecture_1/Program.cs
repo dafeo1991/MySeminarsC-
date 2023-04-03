9dc7fdbe4fc5329bb3bbb97aec303b370bedd7b5
darya <feoktistova.darya2017@yandex.ru>
2023-04-04 00:48:08 +0700

    commit2

--- lecture_1/Program.cs
+++ lecture_1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World and Darya!");
+﻿
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);

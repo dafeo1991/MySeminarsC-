ef7fc8232d788f9343b083d3f5f43a2f34a5906c
darya <feoktistova.darya2017@yandex.ru>
2023-04-04 00:58:07 +0700

    commit3

--- lecture_1/Program.cs
+++ lecture_1/Program.cs
@@ -1,5 +1,11 @@
-﻿
-Console.Write("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(username);
+﻿// Приветствие пользователя
+// Console.Write("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.Write("Hello, ");
+// Console.Write(username);
+
+// Сложение двух чисел
+
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);

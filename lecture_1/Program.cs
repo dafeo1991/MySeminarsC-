4b837659867bd750cf9f25716b6045252637f5d1
darya <feoktistova.darya2017@yandex.ru>
2023-04-09 14:18:54 +0700

    commit12

--- lecture_1/Program.cs
+++ lecture_1/Program.cs
@@ -29,15 +29,84 @@
 
 // Поприветствовать любимого пользователя
 
-Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
+// Console.WriteLine("Введите имя пользователя: ");
+// string username = Console.ReadLine();
 
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine("Ура, это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+// if (username.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username);
+// }
+
+// Поиск самой большой гири
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+
+// int max = a;
+
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+
+// Console.Write("Самая большая гиря весит ");
+// Console.WriteLine(max);
+
+
+// Рисуем треугольник
+
+// Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+// int xa = 40, ya = 1,
+//     xb = 1, yb = 30,
+//     xc = 80, yc = 30;
+
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
+
+// int x = xa, y = xb;
+
+// int count = 0;
+
+// while (count < 10000)
+// {
+//     int what = new Random().Next(0, 3);
+//     if (what == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+//     if (what == 1)
+//     {
+//         x = (x + xb) / 2;
+//         y = (y + yb) / 2;
+//     }
+//     if (what == 2)
+//     {
+//         x = (x + xc) / 2;
+//         y = (y + yc) / 2;
+//     }
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine("+");
+//     count = count + 1;
+// }
+
+
+
+
--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Запросить число и вывести его квадрат
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.Write("Квадрат числа " + num + " -> " + quad);
+// Console.Write($"Квадрат числа {num} -> {quad}");
+
+//2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (quad == num1)
+// {
+//     Console.WriteLine($"Число {num1} является квадратом {num2}");
+// }
+
+// else
+// {
+//     Console.WriteLine($"Число {num1} не квадрат числа {num2}");
+// }
+
+//3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2 = num * -1;
+
+// if (num > 0)
+// {
+//     while (num >= num2)
+//     {
+//         Console.Write($"{num2}, ");
+//         num2 = num2 + 1;
+//     }
+// }
+
+// else if (num < 0)
+// {
+//     while (num <= num2)
+//     {
+//         Console.Write($"{num2 * -1}, ");
+//         num2 = num2 - 1;
+//     }
+
+// }
+
+// else 
+// {
+//     Console.WriteLine("Это ноль");
+// }
+
+
+//4. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// способ 1
+// int n = num % 10;
+// Console.WriteLine($"У числа {num} последняя цифра {n}");
+
+// способ 2
+// Console.WriteLine(Math.Abs(num)%10);

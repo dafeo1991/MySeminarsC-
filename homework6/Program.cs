4a2030ddc03a21063758af68a61fd4e7246bc67c
darya <feoktistova.darya2017@yandex.ru>
2023-05-01 14:54:34 +0700

    01.05.2023

---
+++ homework6/Program.cs
@@ -0,0 +1,78 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int[] MyArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("Получившийся массив -> ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int PositiveNumbers (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void Task1 ()
+{
+    Console.Write("Введите количество элементов в массиве: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    int [] numbers = MyArray(size);
+    PrintArray(numbers);
+    int countNumbers = PositiveNumbers(numbers);
+    Console.WriteLine($"Количество положительных элементов в массиве -> {countNumbers}");
+}
+
+//Task1();
+
+//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+ void Coordinates(double k1, double b1, double k2, double b2)
+{
+
+    double x = (b2 - b1) / (k1 - k2);
+    // double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых -> x {x}, y {y}");
+
+}
+
+void Task2 ()
+{
+Console.WriteLine("Введите координаты k1, b1, k2, b2");
+double k1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
+Coordinates(k1, b1, k2, b2);
+}
+
+Task2 ();
+
--- seminar_6/Program.cs
+++ seminar_6/Program.cs
@@ -1,35 +1,314 @@
-﻿//Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
-// каждая сторона треугольника меньше суммы двух других сторон.
+﻿// семинар с Архипом
 
-// void Triangle (int num1, int num2, int num3)
+// Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте,
+//а первый - на последнем и т.д.)
+
+// int[] MyArray(int size)
 // {
-//     if(num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2)
-//     Console.WriteLine("Такой треугольник существует");
-//     else
-//     Console.WriteLine("Такого треугольника нет");
+//     int[] array = new int[size];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.Next(1, 100);
+//     }
+//     return array;
 // }
 
-// Console.WriteLine("Введите первую сторону");
-// int n1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите вторую сторону");
-// int n2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите третью сторону");
-// int n3 = Convert.ToInt32(Console.ReadLine());
+// void PrintArray(int[] array)
+// {
+//     Console.Write("Получившийся массив -> ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
 
-// Triangle(n1, n2, n3);
+// int[] Revers(int[] array)
+// {
+//     Console.Write("Перевернутый массив -> ");
+//     for (int i = 0, j = array.Length - 1; i < j; i++, j--) // ; отделяет блоки цикла: 1. создали переменные 2. указали условия завершения 3. что происходит 
+//     {                                                      // в каждой итерации
+//         int temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//     }
+//     return array;
+// }
+
+// Console.Write("Введите количество элементов: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// MyArray(size);
+// int[] arr = MyArray(size);
+
+// PrintArray(arr);
+
+// PrintArray(Revers(arr));
+
+// Задача 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// треугольник существует если его сторона меньше суммы двух других
+
+// bool CheckTriangle(int a, int b, int c)
+// {
+//     if (a + b > c && b + c > a && a + c > b)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// int CheckNumber()
+// {
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     if (x <= 0) return CheckNumber();
+//     else return x;
+// }
+
+// Console.Write("Введите сторону А: ");
+// int a = CheckNumber();
+// Console.Write("Введите сторону B: ");
+// int b = CheckNumber();
+// Console.Write("Введите сторону C: ");
+// int c = CheckNumber();
+
+// Console.WriteLine(CheckTriangle(a, b, c));
+
+// Задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+
+// int[] Fibonachii()
+// {
+//     Console.Write("Введите первое число: ");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите второе число: ");
+//     int b = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите последнее число: ");
+//     int n = Convert.ToInt32(Console.ReadLine());
+
+//     int[] array = new int[n];
+//     array[0] = a;
+//     array[1] = b;
+
+//     for (int i = 2; i < n; i++)
+//     {
+//         array[i] = array[i - 1] + array[i - 2];
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintArray(Fibonachii());
+
+// Задача 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// string TransformToBinary (int num)
+// {
+//     string result = String.Empty;
+//     int bin = 0;
+//     while (num > 0)
+//     {
+//         bin = num % 2;
+//         result = bin + result;
+//         num = num / 2;
+//     }
+//     return result;
+// }
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Число {a} в двоичной системе -> {TransformToBinary(a)}");
+
+// Семинар с Денисом
 
 
-void Num (int n) 
-{   
-    int res = 0;
-    while (n > 0)
+void FillArray(int[] array, int minValue = -9, int maxValue = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
     {
-        res = n % 2;
-        n = n / 2;
-        
-        Console.Write(res);
+        array[i] = rnd.Next(minValue, maxValue + 1);
     }
-   
 }
 
-Num (2);
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void Reverse(int[] array)
+{
+    int halfSize = array.Length / 2;
+    int lastIndex = array.Length - 1; // последний элемент в массиве
+
+    for (int i = 0; i < halfSize; i++)
+    {
+        //(array[i], array[lastIndex - i]) = (array[lastIndex - i], array[i]); // то же самое с помощью кортежа
+        // как было                         как стало
+        int temp = array[i];
+        array[i] = array[lastIndex - i];
+        array[lastIndex - i] = temp;
+    }
+
+}
+
+void PrintArrayReverse(int[] array)  // печатаем сразу перевернутый массив
+{
+    int lastIndex = array.Length - 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[lastIndex - i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string Input(string text)
+{
+    Console.Write($"{text} ");
+    return Console.ReadLine();
+}
+
+bool SideCheck(int a, int b, int c)
+{
+    return a + b > c && a + c > b && c + b > a;
+}
+
+(int, int) Test()  // кортеж
+{
+    int x = 6;
+    int y = 8;
+    return (x, y);
+}
+
+void TaskTest() // кортеж
+{
+    Console.WriteLine(Test());
+    int num1;
+    int num2;
+    (num1, num2) = Test();
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+
+    Console.WriteLine(num1 + num2);
+}
+//TaskTest();
+
+// Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.). Решите задачу без создания второго массива.
+
+
+void Task1()
+{
+    int size = 6;
+    int[] numbers = new int[size];
+
+    FillArray(numbers);                        // создает
+    Console.Write("Получившийся массив -> ");
+    PrintArray(numbers);
+
+    Reverse(numbers);                          // только переворачивает
+    Console.Write("Перевернутый массив -> ");
+    PrintArray(numbers);                       // только печатает  
+
+    Console.Write("Печатаем сразу перевернутый массив -> ");
+    PrintArrayReverse(numbers);                // переворачивает и сразу печатает                
+
+
+}
+
+//Task1();
+
+// Задача 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+
+void Task2()
+{
+    int sideA = Convert.ToInt32(Input("Введите сторону А: "));
+    int sideB = Convert.ToInt32(Input("Введите сторону B: "));
+    int sideC = Convert.ToInt32(Input("Введите сторону C: "));
+
+    if (SideCheck(sideA, sideB, sideC))
+        Console.WriteLine("Треугольник с данными сторонами существует");
+    else
+        Console.WriteLine("Треугольник не существует");
+}
+
+// Task2();
+
+// Задача 3. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+void Task3()
+{
+    int number = Convert.ToInt32(Input("Введите число: "));
+    int value = number;
+    int resultInt = 0;       // хранится результат в числе
+    int shift = 1;           // переменная в которую заложен разряд
+    string resultStr = "";   // хранится результат в виде строки
+
+    while (value > 0)
+    {
+        resultInt = resultInt + value % 2 * shift; // для записи наоборот итоговое число + остаток от деления * на разряд
+        resultStr = value % 2 + resultStr;         // сохраняем в строку, новое значение записывается слева
+        shift = shift * 10;
+        value = value / 2;
+    }
+    Console.WriteLine($"Число в двоичном представлении в формате числа {resultInt}");
+    Console.WriteLine($"Число в двоичном представлении в формате строки {resultStr}");
+}
+
+//Task3();
+
+// Задача 4. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+void Task4()
+{
+    int num_f = 0;
+    int num_s = 1;
+    int count = Convert.ToInt32(Input("Количество элементов последовательности: "));
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{num_f} ");
+        (num_f, num_s) = (num_s, num_f + num_s);      // вывод кортежем
+        // int temp = num_f;                          // обычный вывод
+        // num_f = num_s; 
+        // num_s = temp + num_f; 
+    }
+}
+
+//Task4();
+
+// Задача 5. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void Task5()
+{
+    int size = 6;
+    int[] numbers = new int[size];
+
+    FillArray(numbers);
+    Console.Write("Первый массив -> ");
+    PrintArray(numbers);
+
+    int[] numbers_copy =  new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers_copy[i] = numbers[i];
+    }
+
+    numbers[0] = 10000;
+    Console.Write("Копия массив -> ");
+    PrintArray(numbers);
+}
+
+Task5();
